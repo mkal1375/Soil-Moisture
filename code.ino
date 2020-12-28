4852110c7c3118f87a1804f2cdf726bf75aecbca
Mahdi Kalhor <mkal1375@gmail.com>
2020-12-28 16:50:52 +0330

    Add cicuit image

--- code.ino
+++ code.ino
@@ -1,47 +1,52 @@
-int led = 9;
+int led_analog = 9;
+int led_dry = 7;
+int led_humid = 8;
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(A0, INPUT);
-  pinMode(9, OUTPUT);
+  pinMode(led_analog, OUTPUT);
+  pinMode(led_dry, OUTPUT);
+  pinMode(led_humid, OUTPUT);
 }
 
 void loop()
-
 {
-
   int mois = analogRead(A0);
   Serial.print(mois);
   Serial.print(" -");
-
-  if (mois >= 1000) {
-    Serial.println("Sensor is not in the Soil or DISCONNECTED");
-  }
-
-  if (mois < 1000 &&
-    mois >= 600) {
-    Serial.println("Soil is DRY");
-  }
-
-  if (mois < 600 &&
-    mois >= 370) {
-    Serial.println("Soil is HUMID");
-  }
-
-  if (mois < 370) {
-    Serial.println("Sensor in WATER");
-  }
   
   // map 0-1023 to 0-255
-  int brightness = (255*mois)/1023;
-  Serial.println(mois);
-  Serial.println(brightness);
-  if(mois > 400){
-   analogWrite(led, brightness); 
-  }else {
-    analogWrite(led, 0);
+  int brightness = (255 * mois) / 1023; 
+
+  analogWrite(led_analog, brightness);
+  
+  if (mois >= 1000) {
+    Serial.println("Sensor is not in the Soil or DISCONNECTED");
+    digitalWrite(led_dry,LOW);
+    digitalWrite(led_humid,LOW);
+
+  }else if (mois < 1000 && mois >= 600) {
+    
+    Serial.println("Soil is DRY");
+    digitalWrite(led_dry,HIGH);
+    digitalWrite(led_humid,LOW);
+
+  }else if (mois < 600 &&  mois >= 350) {
+    
+    digitalWrite(led_dry,LOW);
+    digitalWrite(led_humid,HIGH);
+    Serial.println("Soil is HUMID");
+
+  }else if (mois < 350) {
+
+    digitalWrite(led_dry,HIGH);
+    digitalWrite(led_humid,HIGH);
+    Serial.println("Sensor in WATER");
+
   }
+
   delay(1000);
 
 }
